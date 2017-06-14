db3b20055392611ce03b1f70607b18d68ee24b2e
dxqdgithub <63101130@qq.com>
2017-06-14 13:35:01 +0800

    修改了一个错误

--- work4.c
+++ work4.c
@@ -2,16 +2,18 @@
 main ()
 {
     int a,b=2,r;
-    printf("请输入一个大于2的数:\n");
+    printf("请输入一个大于2的整数:\n");
     scanf("%d",&a);
     begin:
     if(b<a)
     {
         r=a%b;
-        if(r=0)
-        printf("%d不是素数\n",a);
+        if(r==0)
+        {printf("%d不是素数\n",a);
+        return 0;}
         else b=b+1;
         goto begin;
         }
     else printf("%d是素数\n",a);
+    return 0;
 }
